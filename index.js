a34236da63bd3f421189fb3e111ca83c726cbeff
YasharF <YasharF_Junk3@ydftech.com>
2015-08-26 19:54:02 -0700

    adding getPicture function for pulling from flicker publick images.

--- index.js
+++ index.js
@@ -54,3 +54,48 @@ var server = app.listen(app.get('port'), function () {
   console.log('Example app listening at http://%s:%s', host, port);
 });
 
+
+function getPicture(tags, cb) {
+    var apiKey = "9cf527bef89cfed17e94d77d6234ad31"; // replace this with your API key
+
+    // get an array of random photos
+    $.getJSON(
+        "https://api.flickr.com/services/rest/?jsoncallback=?", {
+            method: 'flickr.photos.search',
+            tags: tags,
+            api_key: apiKey,
+            format: 'json',
+            per_page: 10 // you can increase this to get a bigger array
+        },
+        function(data) {
+
+            // if everything went good
+            if (data.stat == 'ok') {
+                // get a random id from the array
+                var photo = data.photos.photo[Math.floor(Math.random() * data.photos.photo.length)];
+
+                // now call the flickr API and get the picture with a nice size
+                $.getJSON(
+                    "https://api.flickr.com/services/rest/?jsoncallback=?", {
+                        method: 'flickr.photos.getSizes',
+                        api_key: apiKey,
+                        photo_id: photo.id,
+                        format: 'json'
+                    },
+                    function(response) {
+                        if (response.stat == 'ok') {
+                            var the_url = response.sizes.size[5].source;
+                            cb(the_url);
+                        } else {
+                            console.log(" The request to get the picture was not good :\ ")
+                        }
+                    }
+                );
+
+            } else {
+                console.log(" The request to get the array was not good :( ");
+            }
+        }
+    );
+};
+
