53fc5a15a9e59e2ef39ae6035d3f68fb4e67ee69
Hayk Saakian <hayk@skyrealre.com>
2015-08-26 20:08:46 -0700

    fixed images

--- index.js
+++ index.js
@@ -37,23 +37,27 @@ function handler (width, height, req, res) {
     errorHandler(req, res)
     return
   }
-  easyimg.resize({
-       src:'public/img/intro-bg.jpg', dst:'./tmp/'+width+'x'+height+'.jpg',
-       width: width, height: height,
-       cropwidth:width, cropheight:height,
-       ignoreAspectRatio: true,
-       x:0, y:0
-    }).then(
-    function(image) {
-      console.log('Resized and cropped: ' + image.width + ' x ' + image.height);
-      console.log('sending:', image.path)
-      res.sendFile(image.path, { root: __dirname })
-    },
-    function (err) {
-      errorHandler(req, res)
-      console.log(err);
-    }
-  );
+
+  getPicture("horse", function(url){
+    easyimg.resize({
+         src:url, dst:'./tmp/'+width+'x'+height+'.jpg',
+         width: width, height: height,
+         cropwidth:width, cropheight:height,
+         ignoreAspectRatio: true,
+         x:0, y:0
+      }).then(
+      function(image) {
+        console.log('Resized and cropped: ' + image.width + ' x ' + image.height);
+        console.log('sending:', image.path)
+        res.sendFile(image.path, { root: __dirname })
+      },
+      function (err) {
+        errorHandler(req, res)
+        console.log(err);
+      }
+    );
+  })
+  
 }
 
 function errorHandler (req, res) {
@@ -67,8 +71,31 @@ var server = app.listen(app.get('port'), function () {
   console.log('Example app listening at http://%s:%s', host, port);
 });
 
+function getPicture(tags, cb){
+  var list = [
+    "https://farm4.staticflickr.com/3098/2673737268_a355ce2669.jpg",
+    "https://farm6.staticflickr.com/5807/20290758104_5b337f7d5f.jpg",
+    "https://farm1.staticflickr.com/619/20292294883_5824bb5e85.jpg",
+    "https://farm6.staticflickr.com/5690/20920637291_1afab34330.jpg",
+    "https://farm6.staticflickr.com/5686/20877295655_b181a80214.jpg",
+    "https://farm1.staticflickr.com/675/20867598662_3b93d891d3.jpg",
+    "https://farm8.staticflickr.com/7334/10817666133_287b4a3b8c.jpg",
+    "https://farm1.staticflickr.com/647/20709403240_d537cae257.jpg",
+    "https://farm6.staticflickr.com/5653/20909257771_21dae0c6f8.jpg",
+    "https://farm8.staticflickr.com/7632/17126208322_58be639ea0.jpg",
+    "https://farm3.staticflickr.com/2435/3539592423_9c05fa3f3d.jpg",
+    "https://farm6.staticflickr.com/5518/10178207295_85ecb7a2df.jpg"
 
-function getPicture(tags, cb) {
+  ]
+
+  var randomIndex = Math.floor(Math.random() * list.length);
+
+  var randomString = list[randomIndex];
+
+  cb(randomString)
+}
+
+function smartGetPicture(tags, cb) {
     var apiKey = "9cf527bef89cfed17e94d77d6234ad31"; // replace this with your API key
 
     // get an array of random photos
